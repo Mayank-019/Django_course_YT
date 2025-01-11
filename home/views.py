ae383fa2d2ef081026b8fda2e65471ff89f48136
Mayank Kanojia <mayank.kanojia11@gmail.com>
2025-01-11 15:30:36 +0530

    Views and Urls

--- home/views.py
+++ home/views.py
@@ -1,5 +1,20 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+
+from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("This is index page")
+
+# def home(request):
+#     return HttpResponse("""<h1>Hey I am Django server.<h1>
+#         <p>Hey this is coming from Django Server.<p>
+#         <hr>
+#         <h3 style="color:red">Hope you are loving it.<h3>
+                        
+#     """)      # not feasible for complex websites
+
+def home(request):
+    return render(request, "home/index.html")   # returning html file
+
+def success_page(request):
+    print("*" * 10)
+    return HttpResponse("<h1>Hey this is success page.<h1>")
