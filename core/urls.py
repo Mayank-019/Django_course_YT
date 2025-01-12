d942bfe13c2aa2631e24391a2a9a0cb0f9e81379
Mayank Kanojia <mayank.kanojia11@gmail.com>
2025-01-12 17:44:17 +0530

    Template Engine

--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,8 @@ from home.views import *
 urlpatterns = [
     path('', home, name="home"),
     path('success-page/', success_page, name="success_page"),
+    path('contact/', contact, name="contact"),
+    path('about/', about, name="about"),
 
     path('admin/', admin.site.urls),
 ]
--- home/views.py
+++ home/views.py
@@ -13,7 +13,40 @@ from django.http import HttpResponse
 #     """)      # not feasible for complex websites
 
 def home(request):
-    return render(request, "home/index.html")   # returning html file
+    peoples = [
+        {'name' : "Mayank Kanojia", 'age' : 21},
+        {'name' : "Abhijeet Gupta", 'age' : 26},
+        {'name' : "Rohan Verma", 'age' : 16},
+        {'name' : "John Doe", 'age' : 14},
+        {'name' : "Sandeep Sharma", 'age' : 32},
+    ]
+
+    for people in peoples:
+        if people['age'] > 18:
+            print("Yes")
+
+    # for people in peoples:
+    #     print(people)
+
+    # text = """
+    #     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel doloribus voluptatibus doloremque exercitationem nam 
+    #     sed incidunt quaerat sapiente maxime dolore, aliquam iusto alias quos veniam repudiandae inventore, 
+    #     deleniti animi modi?
+    # """
+
+    vegetables = ['Pumpkin', 'Tomato', 'Potato']
+
+    return render(request, "home/index.html", context = {'page' : 'Django Tutorial 2024', 'peoples': peoples, 'vegetables' : vegetables})   # returning html file
+
+
+def about(request):
+    context = {'page' : 'About'}
+    return render(request, "home/about.html", context)   # returning html file
+
+
+def contact(request):
+    context = {'page' : 'Contact'}
+    return render(request, "home/contact.html", context)   # returning html file
 
 def success_page(request):
     print("*" * 10)
