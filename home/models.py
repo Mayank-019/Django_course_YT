3c4caece7bee232478351e4b2d791706ec259872
Mayank Kanojia <mayank.kanojia11@gmail.com>
2025-01-15 21:56:08 +0530

    Models and migration

--- home/models.py
+++ home/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    # id = models.AutoField()   # ye django apne aap add karta h -> primary key jo counter chalte h
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField(null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
+    
+
+class Product(models.Model):
+    pass
