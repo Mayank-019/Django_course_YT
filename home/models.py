8b845e46e18d4b940ce3dab4eccd3fb17af89361
Mayank Kanojia <mayank.kanojia11@gmail.com>
2025-01-16 23:44:53 +0530

    CRUD Operations

--- home/models.py
+++ home/models.py
@@ -10,5 +10,10 @@ class Student(models.Model):
     address = models.TextField(null=True, blank=True)
     
 
-class Product(models.Model):
-    pass
+
+class Car(models.Model):
+    car_name = models.CharField(max_length=100)
+    speed = models.IntegerField(default=50)
+
+    def __str__(self) -> str:
+        return self.car_name
