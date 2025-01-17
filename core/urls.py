df65d985bece2e1f107a6d3b8a7967a35dc78152
Mayank Kanojia <mayank.kanojia11@gmail.com>
2025-01-18 00:53:20 +0530

    Receipe Project

--- core/urls.py
+++ core/urls.py
@@ -18,12 +18,22 @@ from django.contrib import admin
 from django.urls import path
 
 from home.views import *
+from vege.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', home, name="home"),
+    path('receipes/', receipes, name="receipes"),
     path('success-page/', success_page, name="success_page"),
     path('contact/', contact, name="contact"),
     path('about/', about, name="about"),
-
+    
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
